a69d11e90df89b6b4d51b4b46fdb5262ba116389
ikook <ikook.dev@gmail.com>
2021-11-30 21:26:06 +0800

    编写一个 example，看看 sqlparser 的用法

---
+++ examples/dialect.rs
@@ -0,0 +1,13 @@
+use sqlparser::{dialect::GenericDialect, parser::Parser};
+
+fn main() {
+    tracing_subscriber::fmt::init();
+
+    let sql = "select a a1, b, 123, myfunc(b), * \
+    FROM data_source \
+    where a > b and b < 100 and c between 10 and 20 \
+    order by a desc, b limit 50 offset 10";
+
+    let ast = Parser::parse_sql(&GenericDialect::default(), sql);
+    println!("{:#?}", ast);
+}
